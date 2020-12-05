49e5c9861acbfc02fa5a3d72a4ebaf46e83ea469
Darcanaith <darcanaiz@gmail.com>
2020-12-05 17:28:49 +0100

    Monopoly

--- src/monopoly/Casilla.java
+++ src/monopoly/Casilla.java
@@ -1,6 +1,8 @@
 package monopoly;
 
 public interface Casilla {
+    Integer Posicion_tablero();
+    String Nombre_jugador();
     Integer precio_compra();
     Integer precio_alquiler();
 }
--- src/monopoly/Servicio_Privado.java
+++ src/monopoly/Servicio_Privado.java
@@ -2,6 +2,16 @@ package monopoly;
 
 public  class Servicio_Privado implements Casilla {
     @Override
+    public Integer Posicion_tablero() {
+        return null;
+    }
+
+    @Override
+    public String Nombre_jugador() {
+        return null;
+    }
+
+    @Override
     public Integer precio_compra() {
         return null;
     }
